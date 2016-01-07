8f5bb425212c71485b9e1b6124f6b2a294108ea1
Gail Matsushima <gail.matsushima@gmail.com>
2016-01-07 12:07:59 -1000

    got lag event created need to still refine lagTime

--- deltaForce.js
+++ deltaForce.js
@@ -3,39 +3,60 @@ var util = require('util');
 
 module.exports = Timer;
 
-function Timer () {
-  //
+function Timer (max, maxOff) {
+  //comes with a lot of properties that can be inherited
+  //allows us to use/create self.emit
   EventEmitter.call(this);
   //self happens before this, and is the previous-case
   //self is what goes through setInterval
   //'this' is the instance of, the current and now new value
   var self = this;
   var counter = 0;
+  var timerStarter;
+  var maxy = 5 || max;
+  var maxDevy = 50 || maxOff;
+  var stopTime;
+  var startTime;
+  var totalTime;
+  var lagTime;
+
 
   this.start = function () {
+    startTime = Date.now();
     //returning time in MilliSeconds (google more on)
-    var time = Date.now();
 
-    self.emit('start', { startTime : time});
-    setInterval (function () {
+    self.emit('start', { startTime : startTime});
+    timerStarter = setInterval (function () {
       //++ before increments before passing along, since starting at 0
       self.emit('tick', { interval : ++counter});
+      if (counter > maxy) {
+        //use 'self' over 'this' bc self is inside of 'this'; to access Timer
+        self.stop();
+        // self.complete();
+      }
     }, 1000);
+  };
+  this.stop = function() {
+    stopTime = Date.now();
+    // totalTime = Date.now();
+    var lagDiff = counter * 1000;
 
-    if (this.count > 5){
 
+    self.emit('stop', { stopTime : stopTime});
+    //check with Tony if moving totalTime into stopTime was a good solution?
+    //probably not since self.complete() won't be used?
+    totalTime = stopTime - startTime;
+    lagTime = totalTime - lagDiff;
+    self.emit('complete', {totalTime : totalTime});
+    if (lagTime > 20 && lagTime < 30){
+    self.emit('lag', {lagTime : lagTime});
     }
-
-    };
-
-  //   this.stop = function() {
-  //   var time = Date.now();
-
-  //   self.emit('stop', { stopTime : time});
-  //   setInterval (function () {
-  //     self.emit()
-  //   })
-  // };
+    if (lagTime > 30){
+      return 950;
+    }
+    //clears, stops
+    clearInterval(timerStarter);
+  };
 }
 //need to extend before creating a new instance
 util.inherits(Timer, EventEmitter);
@@ -49,8 +70,21 @@ function tickHandler(event) {
   // \n means new line
   process.stdout.write('tick : ' + event.interval + '\n');
 }
+function stopHandler (event) {
+  process.stdout.write('stopTime : ' + event.stopTime + '\n');
+}
+function totalHandler (event) {
+  process.stdout.write('totalTime : ' + event.totalTime + '\n');
+}
+function lagHandler (event){
+  process.stdout.write('lagTime : ' + event.lagTime + '\n');
+}
 
 //what it's listening for and what event
 myTimer.addListener('start', startHandler);
 myTimer.addListener('tick', tickHandler);
+myTimer.addListener('complete', totalHandler);
+myTimer.addListener('lag', lagHandler);
+myTimer.addListener('stop', stopHandler);
+//only need to exclusively call start
 myTimer.start();
